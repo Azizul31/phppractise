c17611d06ecbbcbf598f2f014237b8a03582f665
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2018-12-28 00:58:54 +0600

    indentation code added

---
+++ phpindentation.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Azizul56
+ * Date: 12/28/2018
+ * Time: 12:40 AM
+ */
+$name='Azizul';
+$name1=strtolower($name);
+$age=21;
+if($name1==="azizul")
+    {
+        if($age >=21)
+{
+    echo 'Yes he is azizul';
+    if(1===1)
+    {
+      echo 'Yes 1 is equal to 1';
+    }
+}
+    }
+    else
+{
+    echo 'No he is not azizul';
+}
+?>
