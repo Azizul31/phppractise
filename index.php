eaec226013c71148fc3dc172837d607ece47037e
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2018-12-28 11:22:23 +0600

    assignment operation

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-/* if else statement */
-if (0)
-{
-    echo "Hello people";
-}
-else echo "Hello Bangladesh";
+/* assignment operation */
+$var1=12;
+$var2=13;
+$text1="Azizul Hakim";
+$text2="Chowdhury";
+
+echo $var2+=$var1;
+echo $text1.$text2;
+
