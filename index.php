694997ace747f43274b5acdcd748743794602fd6
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2018-12-28 14:35:20 +0600

    === operation

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-/* assignment operation */
-$var1=12;
-$var2=13;
-$text1="Azizul Hakim";
-$text2="Chowdhury";
-
-echo $var2+=$var1;
-echo $text1.$text2;
-
+/* comparison operation */
+$var1='1';
+$var2=1;
+if($var2===$var1)
+{
+    echo "ok";
+}
+else echo "Not ok";
