55b250aef88bbe0f0bf5875b43550cb6ab2347f6
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2018-12-29 21:29:53 +0600

    different string related function operations

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
-$string="I am Azizul .";
+$string="I am Azizul & hakim .";
 $string_word_count=str_word_count($string,1,'.');
+$string_half=str_shuffle($string);
+$substr=substr($string,0,6);
+$string_reversed=strrev($string);
 print_r($string_word_count);
+echo $string_half;
+echo strlen($string);
+echo $string_reversed;
