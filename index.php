903b54620c79e49bce662a162c3f4d81054c6c5b
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2018-12-29 22:22:15 +0600

    similiar_text function

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 $string="I am Azizul & hakim .";
+$string2="I am Zahir & hasib";
+similar_text($string,$string2,$result);
 $string_word_count=str_word_count($string,1,'.');
 $string_half=str_shuffle($string);
 $substr=substr($string,0,6);
 $string_reversed=strrev($string);
+echo $result."<br>";
 print_r($string_word_count);
 echo $string_half;
 echo strlen($string);
