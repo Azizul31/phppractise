9447f451af77f4a19a644e5851d88bd198ca06f4
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2018-12-29 20:32:07 +0600

    using global variable in the function

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 /*welcome to function*/
-function myName($name)
-{
-    echo($name);
+$num1=10;$num2=11;
+function addResult()
+{   global $num1,$num2;
+    $num3=$num1+$num2;
+    return $num3;
 }
-echo "My name is ";
-myName("azizul");
+echo addResult();
