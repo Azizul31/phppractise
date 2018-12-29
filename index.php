c375bd009e2eff0cec3157057426d7b691dacb64
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2018-12-30 03:53:11 +0600

    include_once & require_once function

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-include 'headers.inc.php';
+include_once "header.inc.php";
+include 'header.inc.php';
 echo "I am azizul and my favourite dish is ".$var1." and my favourite sports is ".$var2;
