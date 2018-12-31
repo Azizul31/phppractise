ffde1559a58e24f2c36cd1316c8cba13646b60d5
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2018-12-31 22:07:27 +0600

    substr_replace and str_replace functions

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
+/*substr_replace() function*/
 $string="I am Azizul hakim.I am a student.I am feeling good today";
-$find="am";
-$i=0;
-$find_length=strlen($find);
-while($pos=strpos($string,$find,$i))
-{
+$new_string=substr_replace($string,'Rabbis',5,6);
+echo $new_string."<br>";
+/*Output: I am Rabbis hakim.I am a student.I am feeling good today*/
 
-    echo $pos."<br>";
-    $i=$pos+$find_length;
-}
+/*str_replace() function*/
+$string2="I am Azizul hakim.I am a student.I am feeling good today";
+$to_change=array('Azizul','am','I');
+$change=array('Shafiq and Habib','are','We');
+
+$new_string1=str_replace($to_change,$change,$string2);
+echo $new_string1;
+/*Output:We are Shafiq and Habib hakim.We are a student.We are feeling good today*/
