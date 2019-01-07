955783b73073b526f06f858445d80ceace30ad4b
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2019-01-08 03:14:15 +0600

    str_ireplace function(2)

--- index.php
+++ index.php
@@ -9,9 +9,12 @@ if(isset( $_POST['user_input'])&& !empty($_POST['user_input']))
     echo $new_user_input=str_ireplace($find,$replace,$user_input);
 
 }
+//output:a***l is a good boy Azizul is a good boy
 ?>
 <hr>
 <form action="index.php" method="POST">
     <textarea name="user_input" rows="6" cols="30"><?php echo  $user_input ?></textarea><br><br>
     <input type="submit" value="Submit">
 </form>
+
+
