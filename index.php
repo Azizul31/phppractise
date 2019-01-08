ecc304d2b665b82dcf36414f620c29703e92d839
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2019-01-08 23:56:32 +0600

    asnjdnbsdfd

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 
 <?php
-//str_ireplace   function
-if(isset( $_POST['user_input'])&& !empty($_POST['user_input']))
+//find and replace application
+if(isset($_POST["user_input"]) && !empty($_POST["user_input"]))
 {
-    $find=array('azizul','ashik','amit');
-    $replace=array('a***l','a***k','a***t');
-    $user_input=$_POST['user_input'];
-    echo $new_user_input=str_ireplace($find,$replace,$user_input);
-
+    echo $text=$_POST["user_input"];
+    echo $find=$_POST["searchfor"];
+    echo $replace=$_POST["replacewith"];
 }
-//output:a***l is a good boy Azizul is a good boy
+
 ?>
 <hr>
 <form action="index.php" method="POST">
-    <textarea name="user_input" rows="6" cols="30"><?php echo  $user_input ?></textarea><br><br>
-    <input type="submit" value="Submit">
+    <textarea name="user_input" rows="6" cols="30"></textarea><br><br>
+Search for:  <input type="text" name="searchfor"><br><br>
+    Replace with:<input type="text" name="replacewith"><br><br> <input type="submit" value="Submit"><br><br>
 </form>
 
 
