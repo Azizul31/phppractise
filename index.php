febfa8c846c81bc10d490ffb4c4c5c35933deb67
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2019-01-19 02:00:51 +0600

    $GET method

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-//detecting user's browser
-$browser=get_browser(null,true);
-print_r($browser);
-
+if((isset($_GET['day']))&& (isset($_GET['month'])) && (isset($_GET['year'])))
+{
+    $day=$_GET['day'];
+    $month=$_GET['month'];
+    $year=$_GET['year'];
+    if(!empty($day) && !empty($month) && !empty($year))
+    {
+        echo "The date is ".$day." /".$month." /".$year." /";
+    }
+    else echo "Operation failed";
+}?>
+<form action="index.php" method="GET">
+    Day:<br>
+    <input type="text" name="day"><br>
+    Month:<br>
+    <input type="text" name="month"><br>
+    Year:<br>
+    <input type="text" name="year"><br>
+    <input type="submit" value="Submit"><br>
+</form>
