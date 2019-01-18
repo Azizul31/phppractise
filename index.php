b9bcd7f40e44d50c7e951acd5b0cfcd5b1ce5480
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2019-01-19 01:42:31 +0600

    detecting user's browser

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
-//better way to get visitor's ip
-$http_client_ip=$_SERVER["HTTP_CLIENT_IP"];
-$http_x_forwarded_for=$_SERVER["HTTP_X_FORWARDED_FOR"];
-$remote_addr=$_SERVER["REMOTE_ADDR"];
-if(!empty($http_client_ip))
-{
-    $ip_address=$http_client_ip;
-}
-if(!empty($http_x_forwarded_for))
-{
-    $ip_address=$http_x_forwarded_for;
-}
-if(!empty($remote_addr))
-{
-    $ip_address=$remote_addr;
-}
-echo $ip_address;
+//detecting user's browser
+$browser=get_browser(null,true);
+print_r($browser);
+
