f83f4a1a04976a0feee51b1a31b10a070ad0f0c8
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2019-01-18 18:42:27 +0600

    server variable for script name

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-//random number generation
-echo rand(1,6);
-?>
-<form action=index.php" method="POST">
-    <input type="submit" name="roll" value="Roll dice">
-</form>
-
+//server variables and script name
+include 'header.inc.php';
+if(isset($_POST['submit']))
+{
+    echo "Process 1";
+}
