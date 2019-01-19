025a2db61ad7c29be2b9ca85e1ddd32ecf9f0e03
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2019-01-19 18:29:06 +0600

    cookies

--- view.php
+++ view.php
@@ -1,7 +1,4 @@
 <?php
-include "set.php";
-if(isset($_SESSION['username']))
-{
-    echo "Welcome ".$_SESSION['username'];
-}
-else echo "Please log in first";
+//cookie
+echo $_COOKIE['username'];
+?>
