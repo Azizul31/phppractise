3ad231910f0fd0716526e57a66bd7b7eb6a95519
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2019-01-19 16:12:29 +0600

    htmlentities

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
-//post method
-$match="pass123";
-if(isset($_POST["password"]))
+//htmlentitites for security purpose
+if((isset($_GET['day'])) && (isset($_GET['month'])) && (isset($_GET['year'])))
 {
-    if(!empty($_POST["password"]))
+    $day=htmlentities($_GET['day']);
+    $month=htmlentities($_GET['day']);
+    $year=htmlentities($_GET['year']);
+    if((!empty($_GET['day'])) && (!empty($_GET['month'])) && (!empty($_GET['year'])))
     {
-        $password=$_POST["password"];
-        if($password==$match) {
-            echo "this is correct";
-        }
-        else echo "This is incorrect";
+        echo "The date is ".$day."st ".$month." ".$year;
     }
-
 }
 ?>
-<form action="index.php" method="POST">
-    PASSWORD:<br>
-    <input type="password" name="password"><br>
+<form action="index.php" method="GET">
+    Day:<input type="text" name="day">
+    Month:<input type="text" name="month">
+    Year:<input type="text" name="year"><br>
     <input type="submit" value="Submit">
 </form>
