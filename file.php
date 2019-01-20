d6fd2d1eae40e884bc834b9963219d7bf2d14ee1
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2019-01-21 01:35:59 +0600

    explode

--- file.php
+++ file.php
@@ -1,34 +1,9 @@
 <?php
-//handling reading a file
-if(isset($_POST['username']))
+//explode function with file handling example
+$handle=fopen('names.txt','r');
+$read=fread($handle,filesize('names.txt'));
+$names_array=explode(',',$read);
+foreach ($names_array as $names)
 {
-    $username=$_POST['username'];
-    if(!empty($_POST['username']))
-    {
-        $handle=fopen('names.txt','a');
-        fwrite($handle,$username."\n");
-        fclose($handle);
-        $readin=file("names.txt");
-        $count=1;
-        $read_count=count($readin);
-
-        foreach ($readin as $fname)
-        {
-            echo trim($fname) ;
-            if($read_count>$count)
-            {
-                echo '/';
-            }
-            $count++;
-
-        }
-
-    }
-    else echo "Please type a name";
+    echo $names.'<br>';
 }
-?>
-<form action="file.php" method="POST">
-    Username:<br>
-    <input type="text" name="username"><br>
-    <input type="submit" value="Submit">
-</form>
